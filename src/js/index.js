fea585bae8f8154720ac20e61d75b0b1922a409f
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2021-05-10 13:24:22 +0300

    feat: wrote <header> in index.html + styles.scss

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+const demo = () => 'Webpack Boilerplate v5.6.0 - SASS/PostCSS, ES6/7, browser sync, source code listing and more.';
+
+console.log(demo());
+// Ключ: 7bbcabd7451880efd46ec7f3f3b268c2
